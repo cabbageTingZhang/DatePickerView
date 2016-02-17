a041992cd06291238412e8ddd002805054155a2c
yanxuyao <yanxuyao@myfriday.cn>
2016-02-17 16:04:50 +0800

    时间戳注释修改

--- LimitDataPicker/LimitDataPicker/DatePickerView/DatePickerView.h
+++ LimitDataPicker/LimitDataPicker/DatePickerView/DatePickerView.h
@@ -15,7 +15,7 @@
 @property (weak, nonatomic) IBOutlet UIView *actionSheetView;/**< 需要完成动画的图层*/
 
 @property (copy, nonatomic) NSString *datePickerText;/**< 最终选择的日期时间,(这个值,外界可以直接获取到)*/
-@property (assign, nonatomic) double endSendNumber;/**< 最终发送的日期时间戳,(这个值,外界可以直接获取到)*/
+@property (assign, nonatomic) double endSendNumber;/**< 最终发送的日期时间戳, 这个时间戳是你选择的日期拼上当前的小时分钟秒数所形成的时间戳,(这个值,外界可以直接获取到)*/
 @property (strong, nonatomic) NSNumber *endSendAmpm;/**< 最终发送的拾取时间区间,0表示不确定,1表示上午,剩下的以此类推,(这个值,外界可以直接获取)*/
 
 @end
